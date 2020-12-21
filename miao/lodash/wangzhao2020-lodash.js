7fb7c0819ae5739c977d9e7f2b834ad12bbce0ba
xiaowang <992283401@qq.com>
2020-12-21 22:44:18 +0800

    isArray&sum&reverse&sortedIndex

--- miao/lodash/wangzhao2020-lodash.js
+++ miao/lodash/wangzhao2020-lodash.js
@@ -239,6 +239,9 @@ var wangzhao2020 = function () {//将compace函数写到函数里边，最后返
     return m
   }
 
+
+
+
   //重复n次给定字符串
   function repeat(string = '', n = 1) {
     //1.先对字符串进行重复，字符串直接相加即可
@@ -254,21 +257,28 @@ var wangzhao2020 = function () {//将compace函数写到函数里边，最后返
 
   //创建新数组，将array与任何数组或值连接在一起
   function concat(array, ...values) {
-    var ary = array.slice()  //等价于下面的函数
-    // var result = []
-    // for (var i = 0; i < array.length; i++){
-    //   result.push(array[i])
-    // }
-    //先判断values的长度，并对每个元素逐个判断
+    //先新建数组，并将array push进去
+    // var ary = array.slice()//下面功能相同
+
+    var ary = []
+    for (var i = 0; i < array.length; i++) {
+      ary.push(array[i])
+    }
+
+    //...后values的值组成了数组，先对这个数组每个元素进行判断
+    //values分为两类，数值和数组，分别用不同的方法
     for (var i = 0; i < values.length; i++) {
       var a = values[i]
-      if (typeof (a) == 'number') {//如果元素为数值，则直接push
-        ary.push(a)
-      } else if (Array.isArray(a)) {//如果元素为数组，则将数组的每个元素push到array后面
+      if (Array.isArray(a)) {//a为数组，需要将数组的每个元素push到ary中
+        // for (var j = 0; j < a.length; j++) {
+        //   ary.push(a[j])
+        // }下面功能相同
+        a.forEach(it => {//forEach遍历所有数组或对象，并对其完成相对应的操作
+          ary.push(it)  //将a的元素进行遍历，并将每个元素push到ary中
+        })
 
-        for (var j = 0; j < a.length; j++) {
-          ary.push(a[j])
-        }
+      } else if (typeof (a) == 'number') {//a为数值，整体push到ary中
+        ary.push(a) //push可以将数值和字符串添加到原数组中
       }
     }
     return ary
@@ -289,8 +299,66 @@ var wangzhao2020 = function () {//将compace函数写到函数里边，最后返
     return result
   }
   function flatten3(array) {//方法3
-    //思路：
+    //思路：涉及到构造函数，构造函数学完去弄
 
+
+  }
+
+
+
+  //对数组或对象的元素遍历并执行某个操作（函数）
+  //真正写好很难，暂不写
+  function forEach(collection, iteratee) {
+    //iteratee调用3个参数(value,index/key,collection)
+    //先对集合进行遍历
+    for (var i = 0; i < collection; i++) {
+      var a = collection[i]
+      iteratee(a)
+    }
+  }
+
+  //如果value是数组则返回true，否则返回false
+  function isArray(value) {
+    //使用一个函数Object.prototype.toString.call()可以固定的输出"[object xxx]"xxx为各种类型
+    return Object.prototype.toString.call(value) === "[object Array]"
+  }
+
+  //sum 求数组中值的总和
+  function sum(array) {
+    //遍历数组每个元素求和
+    var sum = 0
+    for (var i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum
+  }
+
+  //reverse 反转数组的元素，改变原数组
+  function reverse(array) {
+    //从数组的最后一项，从后往前push到新数组
+    var result = []
+    var a = array.length
+    for (var i = a - 1; i >= 0; i--) {
+
+      result.push(array[i])
+    }
+    return result
+  }
+
+  //sortedIndex(array,value)
+  //将value插入数组中，array为排序数组，value插入排序数组，下标尽量小,最后返回下标
+  //题目使用二进制的方式检索决定value的值,这个不知道怎么弄
+  function sortedIndex(array, value) {
+    //让value和array的元素相比，遇到大于等于value的元素，就返回这个元素的下标
+    var a = true
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] >= value) {
+        a = !true
+        return i
+      }
+    }
+    //如果array所有元素都比value小，那么返回最后一个下标
+    return array.length - 1
   }
 
 
@@ -322,6 +390,14 @@ var wangzhao2020 = function () {//将compace函数写到函数里边，最后返
     repeat,
     flatten,
     concat,
+    isArray,
+    sum,
+    reverse,
+    sortedIndex,
+
+
+
+
 
 
 
